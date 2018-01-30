0eb94e9a26953de64139a5ec7f5d5fd82c4fa753
kristoy0 <kristoy0@hotmail.com>
2018-01-30 11:40:21 +0200

    Söögikalkulaatori täiendamine tingimusega, söögi lõpphinna kalkuleerimise lisamine

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -11,3 +11,11 @@ $soogiHind = 2.55; // söögi hind eurodes
 $soodustusProtsent = 15; // soodustusprotsent
 $toetus = 1.80; // õpilase toetus eurodes
 
+// Söögi lõpphind kalkuleeritakse kui kõik eelnevalt defineeritud
+// muutujad on suuremad kui null
+if ($soogiHind > 0 and $soodustusProtsent > 0 and $toetus > 0) {
+    $loppHind = $soogiHind - ($soogiHind * ($soodustusProtsent / 100)) - $toetus;
+    echo 'Söögi lõpphind õpilasele: '.$loppHind.'<br />';
+} else {
+    echo 'Andmed pole täielikud';
+}
