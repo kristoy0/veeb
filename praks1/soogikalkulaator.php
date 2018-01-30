8b5e74e035eeb632f901f77c06efce523648ea8b
kristoy0 <kristoy0@hotmail.com>
2018-01-30 11:31:17 +0200

    Söögikalkulaatori algväärtuste defineerimine

---
+++ praks1/soogikalkulaator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristo.rood
+ * Date: 30.01.2018
+ * Time: 11:28
+ */
+
+// Seadistame vajalikud muutujad
+$soogiHind = 2.55; // söögi hind eurodes
+$soodustusProtsent = 15; // soodustusprotsent
+$toetus = 1.80; // õpilase toetus eurodes
+
