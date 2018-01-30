daff017ecae7cdab9b074dc5393b4762dde7462e
kristoy0 <kristoy0@hotmail.com>
2018-01-30 11:03:46 +0200

    Muutujate defineerimine

--- praks1/valjund.php
+++ praks1/valjund.php
@@ -6,5 +6,12 @@
  * Time: 10:24
  */
 
+// Teksti väljastamine
 echo 'Aine - veebiprogrammeerimine'.'<br />';
 print 'valikaine';
+
+// Muutujate defineerimine
+$eesNimi = 'Kristo'; // eesnimi, string
+$pereNimi = 'R'; // perenimi, string
+$bussiNumber = 4; //bussinumber, integer
+$soiduKestvus = 0.5; // sõidu kestus, float
