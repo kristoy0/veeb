bc2e631b21b36881af6096abf79a8d0014afbee5
kristoy0 <kristoy0@hotmail.com>
2018-02-14 12:57:13 +0200

    Lisatud numbri arvamise mäng

---
+++ praks1/mang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristo.rood
+ * Date: 14.02.2018
+ * Time: 12:31
+ */
+
+require_once 'funktsioonid.php'; // ligipääs funktsioonid.php failile
+
+$suvalineNumber = 10;
+$pakutudNumber = $_GET['number'];
+
+loeVormFailist('manguvorm.html');
+
+if (!empty($pakutudNumber)) {
+    switch ($pakutudNumber) {
+        case $suvalineNumber:
+            echo "Pakkusid õige numbri";
+            break;
+        case ($pakutudNumber >= $suvalineNumber - 5 and $pakutudNumber < $suvalineNumber):
+            echo 'Pakkusid lähedale (>= -5)';
+            break;
+        case ($pakutudNumber <= $suvalineNumber + 5 and $pakutudNumber > $suvalineNumber):
+            echo 'Pakkusid lähedale (<= +5)';
+            break;
+        case ($pakutudNumber < $suvalineNumber):
+            echo 'Pakkusid liiga vähe';
+            break;
+        case ($pakutudNumber > $suvalineNumber):
+            echo 'Pakkusid liiga palju';
+            break;
+    }
+}
