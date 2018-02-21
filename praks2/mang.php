d3e885e64e152010fec8a376da938dfd94311f73
kristoy0 <kristoy0@hotmail.com>
2018-02-21 09:06:52 +0200

    Lisatud katsete arvu lugemine ning suvalise numbri generatsioon

--- praks2/mang.php
+++ praks2/mang.php
@@ -20,15 +20,27 @@
 
 require_once '../praks1/funktsioonid.php'; // ligipääs funktsioonid.php failile
 
-$suvalineNumber = 10;
+$suvalineNumber = $_POST['suvalineNumber'];
+$suvalineNumber = isset($suvalineNumber) ? $suvalineNumber : rand(1, 50);
+
 $pakutudNumber = $_POST['number'];
 
-loeVormFailist('manguvorm.html');
+$katseteArv = $_POST['katseteArv'];
+$katseteArv = isset($katseteArv) ? ++$katseteArv : 0;
+
+echo '<form action="mang.php" method="post">
+    Sisesta number 1 - 50:<br>
+    <input type="number" max="50" min="1" name="number" value="">
+    <input type="hidden" name="katseteArv" value="'.$katseteArv.'">
+    <input type="hidden" name="suvalineNumber" value="'.$suvalineNumber.'">
+    <input type="submit" value="Arva">
+</form>';
 
 if (!empty($pakutudNumber)) {
     switch ($pakutudNumber) {
         case $suvalineNumber:
-            echo "Pakkusid õige numbri";
+            echo 'Pakkusid õige numbri <br>';
+            echo 'Sul läks arvamiseks '.$katseteArv.' katset';
             break;
         case ($pakutudNumber >= $suvalineNumber - 5 and $pakutudNumber < $suvalineNumber):
             echo 'Pakkusid lähedale (>= -5)';
